0648c64b599fea857de1d344399161401c95efa5
jorge <jorge@MELISA>
2021-09-21 20:37:10 -0600

    Agregar archivos de proyecto.

---
+++ CIBER WEB/PACIENTE.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace CIBER_WEB
+{
+    partial class PACIENTE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PACIENTE));
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar1 = new DevExpress.XtraBars.Bar();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
+            this.bar2 = new DevExpress.XtraBars.Bar();
+            this.bar3 = new DevExpress.XtraBars.Bar();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.empleadosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.colid_empleados = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colNombres = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colApellidos = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colid_direccion = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTelefono = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colid_cargo = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colid_sexo = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCargo = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDireccion = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSexo = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empleadosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1,
+            this.bar2,
+            this.bar3});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barButtonItem1,
+            this.barButtonItem2,
+            this.barButtonItem3});
+            this.barManager1.MainMenu = this.bar1;
+            this.barManager1.MaxItemId = 3;
+            this.barManager1.StatusBar = this.bar3;
+            // 
+            // bar1
+            // 
+            this.bar1.BarName = "MainMenu";
+            this.bar1.DockCol = 0;
+            this.bar1.DockRow = 0;
+            this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem1),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem2, true),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem3, true)});
+            this.bar1.OptionsBar.MultiLine = true;
+            this.bar1.OptionsBar.UseWholeRow = true;
+            this.bar1.Text = "MainMenu";
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "AGREGAR";
+            this.barButtonItem1.Id = 0;
+            this.barButtonItem1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.ImageOptions.Image")));
+            this.barButtonItem1.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.ImageOptions.LargeImage")));
+            this.barButtonItem1.Name = "barButtonItem1";
+            this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick);
+            // 
+            // barButtonItem2
+            // 
+            this.barButtonItem2.Caption = "MODIFICAR";
+            this.barButtonItem2.Id = 1;
+            this.barButtonItem2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem2.ImageOptions.Image")));
+            this.barButtonItem2.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barButtonItem2.ImageOptions.LargeImage")));
+            this.barButtonItem2.Name = "barButtonItem2";
+            // 
+            // barButtonItem3
+            // 
+            this.barButtonItem3.Caption = "ELIMINAR";
+            this.barButtonItem3.Id = 2;
+            this.barButtonItem3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem3.ImageOptions.Image")));
+            this.barButtonItem3.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barButtonItem3.ImageOptions.LargeImage")));
+            this.barButtonItem3.Name = "barButtonItem3";
+            // 
+            // bar2
+            // 
+            this.bar2.BarName = "Tools";
+            this.bar2.DockCol = 0;
+            this.bar2.DockRow = 1;
+            this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar2.Text = "Tools";
+            // 
+            // bar3
+            // 
+            this.bar3.BarName = "StatusBar";
+            this.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+            this.bar3.DockCol = 0;
+            this.bar3.DockRow = 0;
+            this.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+            this.bar3.OptionsBar.AllowQuickCustomization = false;
+            this.bar3.OptionsBar.DrawDragBorder = false;
+            this.bar3.OptionsBar.UseWholeRow = true;
+            this.bar3.Text = "StatusBar";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(797, 45);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 303);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(797, 20);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 45);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 258);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(797, 45);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 258);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.empleadosBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 45);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(797, 258);
+            this.gridControl1.TabIndex = 4;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.gridControl1.Load += new System.EventHandler(this.gridControl1_Load);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colid_empleados,
+            this.colNombres,
+            this.colApellidos,
+            this.colid_direccion,
+            this.colTelefono,
+            this.colid_cargo,
+            this.colid_sexo,
+            this.colCargo,
+            this.colDireccion,
+            this.colSexo});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // empleadosBindingSource
+            // 
+            this.empleadosBindingSource.DataSource = typeof(CIBER_WEB.Empleados);
+            // 
+            // colid_empleados
+            // 
+            this.colid_empleados.FieldName = "id_empleados";
+            this.colid_empleados.Name = "colid_empleados";
+            this.colid_empleados.Visible = true;
+            this.colid_empleados.VisibleIndex = 0;
+            // 
+            // colNombres
+            // 
+            this.colNombres.FieldName = "Nombres";
+            this.colNombres.Name = "colNombres";
+            this.colNombres.Visible = true;
+            this.colNombres.VisibleIndex = 1;
+            // 
+            // colApellidos
+            // 
+            this.colApellidos.FieldName = "Apellidos";
+            this.colApellidos.Name = "colApellidos";
+            this.colApellidos.Visible = true;
+            this.colApellidos.VisibleIndex = 2;
+            // 
+            // colid_direccion
+            // 
+            this.colid_direccion.FieldName = "id_direccion";
+            this.colid_direccion.Name = "colid_direccion";
+            this.colid_direccion.Visible = true;
+            this.colid_direccion.VisibleIndex = 3;
+            // 
+            // colTelefono
+            // 
+            this.colTelefono.FieldName = "Telefono";
+            this.colTelefono.Name = "colTelefono";
+            this.colTelefono.Visible = true;
+            this.colTelefono.VisibleIndex = 4;
+            // 
+            // colid_cargo
+            // 
+            this.colid_cargo.FieldName = "id_cargo";
+            this.colid_cargo.Name = "colid_cargo";
+            this.colid_cargo.Visible = true;
+            this.colid_cargo.VisibleIndex = 5;
+            // 
+            // colid_sexo
+            // 
+            this.colid_sexo.FieldName = "id_sexo";
+            this.colid_sexo.Name = "colid_sexo";
+            this.colid_sexo.Visible = true;
+            this.colid_sexo.VisibleIndex = 6;
+            // 
+            // colCargo
+            // 
+            this.colCargo.FieldName = "Cargo";
+            this.colCargo.Name = "colCargo";
+            this.colCargo.Visible = true;
+            this.colCargo.VisibleIndex = 7;
+            // 
+            // colDireccion
+            // 
+            this.colDireccion.FieldName = "Direccion";
+            this.colDireccion.Name = "colDireccion";
+            this.colDireccion.Visible = true;
+            this.colDireccion.VisibleIndex = 8;
+            // 
+            // colSexo
+            // 
+            this.colSexo.FieldName = "Sexo";
+            this.colSexo.Name = "colSexo";
+            this.colSexo.Visible = true;
+            this.colSexo.VisibleIndex = 9;
+            // 
+            // PACIENTE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 323);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Name = "PACIENTE";
+            this.Text = "PACIENTE";
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empleadosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar1;
+        private DevExpress.XtraBars.Bar bar2;
+        private DevExpress.XtraBars.Bar bar3;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem3;
+        private System.Windows.Forms.BindingSource empleadosBindingSource;
+        private DevExpress.XtraGrid.Columns.GridColumn colid_empleados;
+        private DevExpress.XtraGrid.Columns.GridColumn colNombres;
+        private DevExpress.XtraGrid.Columns.GridColumn colApellidos;
+        private DevExpress.XtraGrid.Columns.GridColumn colid_direccion;
+        private DevExpress.XtraGrid.Columns.GridColumn colTelefono;
+        private DevExpress.XtraGrid.Columns.GridColumn colid_cargo;
+        private DevExpress.XtraGrid.Columns.GridColumn colid_sexo;
+        private DevExpress.XtraGrid.Columns.GridColumn colCargo;
+        private DevExpress.XtraGrid.Columns.GridColumn colDireccion;
+        private DevExpress.XtraGrid.Columns.GridColumn colSexo;
+    }
+}
---
+++ CIBER WEB/PACIENTE.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIBER_WEB
+{
+    public partial class PACIENTE : DevExpress.XtraEditors.XtraForm
+    {
+        public PACIENTE()
+        {
+            InitializeComponent();
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void gridControl1_Load(object sender, EventArgs e)
+        {
+            Empleados db = new Empleados();
+            empleadosBindingSource.DataSource = Empleados.toList();
+
+        }
+    }
+}
